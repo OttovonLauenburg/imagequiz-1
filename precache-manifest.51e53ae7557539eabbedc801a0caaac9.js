e15d772e51a63fd85e6d18c4679dfa0ef7b521b4
habahram <hamid.bahramian@gmail.com>
2020-09-22 16:09:38 -0400

    Updates

---
+++ precache-manifest.51e53ae7557539eabbedc801a0caaac9.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a40394aca5cc37b84c1923a20ffcd208",
+    "url": "/imagequiz/index.html"
+  },
+  {
+    "revision": "9d821a079defdeb7fea0",
+    "url": "/imagequiz/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "558cd267cd6ed04d71e8",
+    "url": "/imagequiz/static/js/2.dec33d34.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/imagequiz/static/js/2.dec33d34.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9d821a079defdeb7fea0",
+    "url": "/imagequiz/static/js/main.44a11797.chunk.js"
+  },
+  {
+    "revision": "85da51fa7abe07fb0938",
+    "url": "/imagequiz/static/js/runtime-main.2f3f5ace.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/imagequiz/static/media/logo.5d5d9eef.svg"
+  }
+]);
